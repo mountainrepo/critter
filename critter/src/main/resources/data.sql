02e013cbd6b395724b3dcc2e557cb44442e6955d
Rajeswari <thinkpuretech@email829.me>
2021-02-21 18:57:15 -0800

    added database initialization configuration

---
+++ critter/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO critter.skill VALUES(1, 'PETTING');
+INSERT INTO critter.skill VALUES(2, 'WALKING');
+INSERT INTO critter.skill VALUES(3, 'FEEDING');
+INSERT INTO critter.skill VALUES(4, 'MEDICATING');
+INSERT INTO critter.skill VALUES(5, 'SHAVING');
+
+INSERT INTO critter.week_day VALUES(1, "MONDAY");
+INSERT INTO critter.week_day VALUES(2, "TUESDAY");
+INSERT INTO critter.week_day VALUES(3, "WEDNESDAY");
+INSERT INTO critter.week_day VALUES(4, "THURSDAY");
+INSERT INTO critter.week_day VALUES(5, "FRIDAY");
+INSERT INTO critter.week_day VALUES(6, "SATURDAY");
+INSERT INTO critter.week_day VALUES(7, "SUNDAY");
